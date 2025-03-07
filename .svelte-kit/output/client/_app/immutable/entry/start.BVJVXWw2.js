150ee94b7895f529a76e7998575f51678af82817
mikostel <mikostel@gmail.com>
2025-03-06 21:45:53 -0500

    Update name

---
+++ .svelte-kit/output/client/_app/immutable/entry/start.BVJVXWw2.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.B5onFIFr.js";export{t as start};
---
+++ .svelte-kit/output/server/manifest-full.js
@@ -0,0 +1,35 @@
+export const manifest = (() => {
+function __memo(fn) {
+	let value;
+	return () => value ??= (value = fn());
+}
+
+return {
+	appDir: "_app",
+	appPath: "_app",
+	assets: new Set([".nojekyll","favicon.png"]),
+	mimeTypes: {".png":"image/png"},
+	_: {
+		client: {"start":"_app/immutable/entry/start.BVJVXWw2.js","app":"_app/immutable/entry/app.DFHUiUl_.js","imports":["_app/immutable/entry/start.BVJVXWw2.js","_app/immutable/chunks/entry.B5onFIFr.js","_app/immutable/chunks/scheduler.W2pu3yam.js","_app/immutable/chunks/paths.xlxQdyih.js","_app/immutable/entry/app.DFHUiUl_.js","_app/immutable/chunks/scheduler.W2pu3yam.js","_app/immutable/chunks/index.RM5oWozK.js"],"stylesheets":[],"fonts":[],"uses_env_dynamic_public":false},
+		nodes: [
+			__memo(() => import('./nodes/0.js')),
+			__memo(() => import('./nodes/1.js')),
+			__memo(() => import('./nodes/2.js'))
+		],
+		routes: [
+			{
+				id: "/",
+				pattern: /^\/$/,
+				params: [],
+				page: { layouts: [0,], errors: [1,], leaf: 2 },
+				endpoint: null
+			}
+		],
+		matchers: async () => {
+			
+			return {  };
+		},
+		server_assets: {}
+	}
+}
+})();
